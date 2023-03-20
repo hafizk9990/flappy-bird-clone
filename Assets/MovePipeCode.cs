915fb9f87f28fbc0bab16c949abf38ba6685f4c8
Uzair Afzal <studies9990@gmail.com>
2023-03-20 23:03:24 +0500

    Spawned pipes will stop moving when the bird is dead

--- Assets/MovePipeCode.cs
+++ Assets/MovePipeCode.cs
@@ -4,10 +4,19 @@ public class MovePipeCode : MonoBehaviour
 {
   private float transformationSpeed = 10;
   private float deadZone = -35;
+  public BirdCode bird;
+
+  void Start()
+  {
+    bird = GameObject.FindGameObjectWithTag("Bird").GetComponent<BirdCode>();
+  }
 
   void Update()
   {
-    transform.position = transform.position + (Vector3.left * transformationSpeed) * Time.deltaTime;
+    if (!bird.isGameOver)
+    {
+      transform.position = transform.position + (Vector3.left * transformationSpeed) * Time.deltaTime;
+    }
 
     if (transform.position.x < deadZone)
     {
